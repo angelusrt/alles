ba1269fc0652b77afee57069ec31be43c2cefda4
Angelus <angelusrt@gmail.com>
2021-06-19 19:57:45 -0300

    💻 Grid layout, responsividade e json adicionados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import { Bar } from 'react-chartjs-2';
+
+import data2 from "./data.json";
+import './App.css'
+
+function Blocks(props) {
+  return (
+    <div className="Blocks" style={props.style}> 
+      {props.children}
+    </div>
+  )
+}
+
+function Section() {
+  const[gridSpan, setGridSpan] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => setGridSpan(window.innerWidth)
+
+    window.addEventListener("resize", handleResize)
+  })
+
+  return(
+    <section>
+      <Blocks style={{
+        gridColumn: gridSpan > 600 ? "auto / span 2" : "auto / span 1",
+        gridRow: gridSpan > 600 ? "auto / span 1" : "auto / span 2"
+      }}>
+        <text className="Blocks-Header">{data2.a[0].header}</text>
+        <text className="Blocks-BodyText">{data2.a[0].bodyText}</text>
+      </Blocks>
+      <Blocks style={{gridColumn: "auto / span 1"}}>
+        <text className="Blocks-Header">{data2.a[1].header}</text>
+        <text className="Blocks-BodyText">{data2.a[1].bodyText}</text>
+      </Blocks>
+      <Blocks style={{gridColumn: "auto / span 1"}}>
+        <Bar 
+          data={data2.b[0].data} 
+          options={{devicePixelRatio: window.devicePixelRatio * 2, ...data2.b[0].options}} 
+          width="100%" 
+          height="100%" 
+        />
+      </Blocks>
+    </section>
+  )
+}
+
+function Banner() {
+  return(
+    <div className="Banner">
+      <text className="Banner-Header">Alles</text>
+      <text className="Banner-Subheader">
+        O estado proto-fenomenal move as sensações da realidade percebida.
+      </text>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <Section/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
